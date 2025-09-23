ec15d4bf5dc62ef841cd942748936f12864b4a93
Carlos Moya <mrcamoga@gmail.com>
2025-09-23 22:08:20 +0200

    Update CompanySection.jsx
    Consistencia de idiomas

--- src/pages/sections/company/CompanySection.jsx
+++ src/pages/sections/company/CompanySection.jsx
@@ -7,14 +7,14 @@ export default function CompanySection() {
 	return (
 		<div className="companySectionContainer">
 			<div>
-				<img src={LogoutIcon} alt="cerrar sesion" width={40} />
+				<img src={LogoutIcon} alt="cerrar sesión" width={40} />
 			</div>
 			<div>
 				<h2>Hola Empresa</h2>
 			</div>
 			<div className="sectionImgContainer">
-				<img src={AddVehicle} alt="add vehiculos" width={200} />
-				<img src={AddDriver} alt="add Drivers" width={180} />
+				<img src={AddVehicle} alt="añadir vehiculos" width={200} />
+				<img src={AddDriver} alt="añadir conductores" width={180} />
 			</div>
 		</div>
 	);
