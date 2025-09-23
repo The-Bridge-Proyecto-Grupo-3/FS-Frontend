256704e5397375c77889902d18397718776a17a4
SantiagoOH21 <soh0621@gmail.com>
2025-09-24 00:19:38 +0200

    toolkit, redux & sass installed & provider added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Principal from './pages/principal/Principal';
 import Layout from './layout/layout';
 import CompanySection from './pages/sections/company/CompanySection';
 import DriverSection from './pages/sections/driver/DriverSection';
+import RegisterDriver from './pages/Register/RegisterDriver';
+
 function App() {
 	return (
 		<Routes>
@@ -11,6 +13,7 @@ function App() {
 			<Route index element={<Principal />} />
 			<Route path="/company" element={<CompanySection />} />
 			<Route path="/driver" element={<DriverSection />} />
+			<Route path="/registerDriver" element={<RegisterDriver />} />
 		</Routes>
 	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<Router>
-			<App />
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<App />
+			</Router>
+		</Provider>
 	</StrictMode>
 );
