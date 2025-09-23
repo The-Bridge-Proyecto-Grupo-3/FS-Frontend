c3f94922ce7c912b9b0ea8631a38f2af918da0af
SantiagoOH21 <soh0621@gmail.com>
2025-09-23 23:44:22 +0200

    Create register in progress

---
+++ src/pages/Register/RegisterDriver.jsx
@@ -0,0 +1,120 @@
+import { useDispatch } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
+import { register } from '../../redux/auth/authSlice';
+import './signForm.scss';
+
+const RegisterDriver = () => {
+	const dispatch = useDispatch();
+
+	const validation = {
+		username: value => {
+			value = value ? value.trim() : '';
+			return [
+				[
+					value.length >= 4 && value.length <= 24,
+					'El nombre debe contener entre 4 y 24 caracteres',
+				],
+			];
+		},
+		screenname: value => [
+			[value && value.trim().length > 0, 'El apellido no puede estar vacío'],
+		],
+		email: value => [
+			[
+				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+					value
+				),
+				'Email no válido',
+			],
+		],
+		password: value => [
+			[/[a-z]+/.test(value), 'La contraseña debe contener minúsculas'],
+			[/[A-Z]+/.test(value), 'La contraseña debe contener mayúsculas'],
+			[/[0-9]+/.test(value), 'La contraseña debe contener números'],
+			[/[^A-Za-z0-9]+/.test(value), 'La contraseña debe contener símbolos especiales'],
+			[value && value.length >= 8, 'La contraseña debe tener 8 caracteres o más'],
+		],
+		// reppassword: value => [[value == formData.password, 'Passwords do not match']],
+	};
+
+	const onSubmit = form => dispatch(register(new FormData(form))).unwrap();
+
+	const { formData, message, success, handleInputChange, handleSubmit } = useForm({
+		validation,
+		onSubmit,
+	});
+
+	return (
+		<div className="signForm">
+			{success ? (
+				<>
+					<h1>Sign in successful!</h1>
+					<p>Please check your email to verify your account.</p>
+				</>
+			) : (
+				<form onSubmit={handleSubmit}>
+					<h1>Registro nuevo de Conductor</h1>
+					<div>
+						<label htmlFor="username">Nombre</label>
+						<input
+							id="username"
+							name="username"
+							className={formData.username && 'validate'}
+							type="text"
+							placeholder="Username"
+							onChange={handleInputChange}
+						/>
+					</div>
+					<div>
+						<label htmlFor="name">Apellidos:</label>
+						<input
+							id="name"
+							name="screenname"
+							className={formData.screenname && 'validate'}
+							type="text"
+							placeholder="Screen name"
+							onChange={handleInputChange}
+						/>
+					</div>
+					<div>
+						<label htmlFor="email">Email</label>
+						<input
+							id="email"
+							name="email"
+							className={formData.email && 'validate'}
+							type="text"
+							placeholder="Email"
+							onChange={handleInputChange}
+						/>
+					</div>
+					<div>
+						<label htmlFor="password">Contraseña</label>
+						<input
+							id="password"
+							name="password"
+							className={formData.password && 'validate'}
+							type="password"
+							placeholder="Password"
+							onChange={handleInputChange}
+						/>
+					</div>
+					{/* <div>
+						<label htmlFor="reppassword">Repeat password:</label>
+						<input
+							id="reppassword"
+							name="reppassword"
+							className={formData.reppassword && 'validate'}
+							type="password"
+							placeholder="Repeat password"
+							onChange={handleInputChange}
+						/>
+					</div> */}
+					<span className="error">{message}</span>
+					<input type="submit" value="Registrar" />
+				</form>
+			)}
+		</div>
+	);
+};
+
+export default RegisterDriver;
---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,19 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import authService from './authService';
+
+const initialState = {
+	user: null,
+	token: null,
+};
+
+export const register = createAsyncThunk('auth/register', async user => {
+	console.log('desde store', user);
+});
+
+export const authSlice = createSlice({
+	name: 'auth',
+	initialState,
+	reducers: {},
+});
+
+export default authSlice.reducer;
