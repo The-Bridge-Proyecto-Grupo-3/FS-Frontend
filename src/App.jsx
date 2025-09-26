32121f4b1c6d206eddba326fde43710b88b5b8ff
SantiagoOH21 <soh0621@gmail.com>
2025-09-26 20:18:54 +0200

    create Receipt working

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Login from './pages/login/login.jsx';
 import VehicleDetails from './components/Vehicles/VehicleDetails';
 import NearbyGastations from './pages/Gastations/NearbyGastations.jsx';
 import ProtectedRoute from './guards/ProtectedRoute.jsx';
+import RegisterReceipt from './pages/Register/RegisterReceipt.jsx';
 
 function App() {
 	return (
@@ -20,13 +21,63 @@ function App() {
 		<Routes>
 			<Route path="/" element={<Layout />} />
 			<Route index element={<Principal />} />
-			<Route path="/admin" element={<ProtectedRoute role='admin' ><AdminSection /></ProtectedRoute>} />
-			<Route path="/company" element={<ProtectedRoute role='company' ><CompanySection /></ProtectedRoute>} />
-			<Route path="/driver" element={<ProtectedRoute role='driver' ><DriverSection /></ProtectedRoute>} />
+			<Route
+				path="/admin"
+				element={
+					<ProtectedRoute role="admin">
+						<AdminSection />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/company"
+				element={
+					<ProtectedRoute role="company">
+						<CompanySection />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/driver"
+				element={
+					<ProtectedRoute role="driver">
+						<DriverSection />
+					</ProtectedRoute>
+				}
+			/>
 			<Route path="/login" element={<Login />} />
-			<Route path="/register" element={<ProtectedRoute><RegisterCompany /></ProtectedRoute>} />
-			<Route path="/registerDriver" element={<ProtectedRoute role='company' ><RegisterDriver /></ProtectedRoute>} />
-			<Route path="/registerVehicle" element={<ProtectedRoute role='company' ><RegisterVehicle /></ProtectedRoute>} />
+			<Route
+				path="/register"
+				element={
+					<ProtectedRoute>
+						<RegisterCompany />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/registerDriver"
+				element={
+					<ProtectedRoute role="company">
+						<RegisterDriver />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/registerVehicle"
+				element={
+					<ProtectedRoute role="company">
+						<RegisterVehicle />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/registerReceipt"
+				element={
+					<ProtectedRoute role="driver">
+						<RegisterReceipt />
+					</ProtectedRoute>
+				}
+			/>
 			<Route path="/vehicles/:id" element={<VehicleDetails />} />
 			<Route path="/searchGS" element={<SearchGS />} />
 			<Route path="/NearbyGS" element={<NearbyGastations />} />
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import auth from '../redux/auth/authSlice';
 import vehicles from '../redux/vehicles/vehicleSlice';
 import oil from '../redux/oilApi/oilSlice';
-import receipt from '../redux/receipts/receiptSlice';
+import receipts from '../redux/receipts/receiptSlice';
 
 export const store = configureStore({
-	reducer: { auth, oil, vehicles, receipt },
+	reducer: { auth, oil, vehicles, receipts },
 });
