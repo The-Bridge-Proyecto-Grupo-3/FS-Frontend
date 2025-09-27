bf9411d4336c36a0fb696f3b6d41e239acfb4936
Carlos Moya <mrcamoga@gmail.com>
2025-09-28 00:24:04 +0200

    Cleaned redirectHome

--- src/redirect/RedirectHome.jsx
+++ src/redirect/RedirectHome.jsx
@@ -1,27 +1,15 @@
 import { useNavigate } from 'react-router-dom';
-
 import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
 
-export default function Layout() {
+export default function RedirectHome() {
 	const navigate = useNavigate();
 	const { role } = useSelector(state => state.auth);
 
 	useEffect(() => {
-		switch (role) {
-			case 'admin':
-				navigate('/admin', { replace: true });
-				break;
-			case 'company':
-				navigate('/company', { replace: true });
-				break;
-			case 'driver':
-				navigate('/driver', { replace: true });
-				break;
-			default:
-				navigate('/login', { replace: true });
-		}
-	},[]);
+		if(role) navigate(`/${role}`, { replace: true });
+		else navigate(`/login`, { replace: true });
+	}, []);
 
 	return;
 }
