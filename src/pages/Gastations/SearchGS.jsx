ae176881c4ec4aed9e3b205c053da3a983760ea4
SantiagoOH21 <soh0621@gmail.com>
2025-09-27 16:23:02 +0200

    BackArrowIcon added

--- src/pages/Gastations/SearchGS.jsx
+++ src/pages/Gastations/SearchGS.jsx
@@ -1,43 +1,49 @@
-import { useEffect } from "react";
-import { useGastations } from "./useGastations"; 
-import { SelectorForm } from "./SelectorForm";
-import { GastationDetails } from "./GastationDetails";
+import { useEffect } from 'react';
+import { useGastations } from './useGastations';
+import { SelectorForm } from './SelectorForm';
+import { GastationDetails } from './GastationDetails';
+import { Link } from 'react-router-dom';
+import BackArrowIcon from '../../assets/BackArrowIcon.png';
 
 const SearchGS = () => {
+	const {
+		provincias,
+		municipios,
+		gastations,
+		gastationDetails,
+		isLoading,
+		isError,
+		message,
+		loadProvincias,
+		selectProvincia,
+		selectMunicipio,
+		selectGastation,
+	} = useGastations();
 
-  const {
-    provincias,
-    municipios,
-    gastations,
-    gastationDetails,
-    isLoading,
-    isError,
-    message,
-    loadProvincias,
-    selectProvincia,
-    selectMunicipio,
-    selectGastation,
-  } = useGastations();
+	useEffect(() => {
+		loadProvincias();
+	}, [loadProvincias]);
 
-  useEffect(() => {
-    loadProvincias();
-  }, [loadProvincias]);
+	const selectorData = { provincias, municipios, gastations };
+	const selectorActions = { selectProvincia, selectMunicipio, selectGastation };
 
-  const selectorData = { provincias, municipios, gastations };
-  const selectorActions = { selectProvincia, selectMunicipio, selectGastation };
+	return (
+		<div>
+			<div className="iconContainer">
+				<Link to="/driver">
+					<img src={BackArrowIcon} alt="atras" width={30} />
+				</Link>
+			</div>
+			<h2>Gasolineras</h2>
 
-  return (
-    <div>
-      <h2>Gasolineras</h2>
+			<SelectorForm data={selectorData} actions={selectorActions} />
 
-      <SelectorForm data={selectorData} actions={selectorActions} />
+			{isLoading && <p>Cargando datos...</p>}
+			{isError && <p style={{ color: 'red' }}>Error: {message}</p>}
 
-      {isLoading && <p>Cargando datos...</p>}
-      {isError && <p style={{ color: "red" }}>Error: {message}</p>}
-
-      <GastationDetails details={gastationDetails} />
-    </div>
-  );
+			<GastationDetails details={gastationDetails} />
+		</div>
+	);
 };
 
 export default SearchGS;
