03e1dc498329b627c3cc903f22a0e389cfd0e44a
IGNA46img <img.872122@gmail.com>
2025-09-29 20:50:49 +0200

    red, yellow, green circles added

--- src/components/IconButton/IconButton.jsx
+++ src/components/IconButton/IconButton.jsx
@@ -2,19 +2,30 @@ import back from '../../assets/BackArrowIcon.png';
 // import edit from '../../assets/EditIcon.png';
 // import deleteIcon from '../../assets/DeleteIcon.png';
 import logout from '../../assets/LogoutIcon.png';
-
+import red from '../../assets/red_circle.png';
+import yellow from '../../assets/yellow_circle.png';
+import green from '../../assets/green_circle.png';
 import './IconButton.scss';
 
 const icons = {
-	back, logout, //edit, delete: deleteIcon
-}
+	back,
+	logout,
+	red,
+	yellow,
+	green, //edit, delete: deleteIcon
+};
 
 const IconButton = ({ icon, title, label, onClick, style }) => {
 	return (
-		<button title={title} className="iconBtn" onClick={onClick} style={{ '--icon-url': `url(${icons[icon]})`, ...style}}>
+		<button
+			title={title}
+			className="iconBtn"
+			onClick={onClick}
+			style={{ '--icon-url': `url(${icons[icon]})`, ...style }}
+		>
 			{label}
 		</button>
-	)
+	);
 };
 
 export default IconButton;
