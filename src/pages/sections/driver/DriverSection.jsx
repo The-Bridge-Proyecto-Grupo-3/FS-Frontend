5ee7e2c556a839009efdcabc217d72efcd0ef1d6
Carlos Moya <mrcamoga@gmail.com>
2025-09-29 13:03:27 +0200

    Added selected vehicle to driver section

--- src/pages/sections/driver/DriverSection.jsx
+++ src/pages/sections/driver/DriverSection.jsx
@@ -1,16 +1,31 @@
 import './driverSection.css';
 import Autocomplete from '@mui/material/Autocomplete';
-import LogoutIcon from '../../../assets/LogoutIcon.png';
 import { TextField } from '@mui/material';
 import FindGass from '../../../assets/FindGass.png';
 import TicketIcon from '../../../assets/TicketIcon.png';
 import FindElecDot from '../../../assets/FindElecDot.png';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import LogoutButton from '../../../components/Buttons/LogoutButton';
+import { useEffect, useMemo } from 'react';
+import { fetchVehicles } from '../../../redux/vehicles/vehicleSlice';
 
 export default function DriverSection() {
 	const { user } = useSelector(state => state.auth);
+	const { vehicles } = useSelector(state => state.vehicles);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(fetchVehicles(true));
+	},[user])
+
+	const vehicleToOption = vehicle => (vehicle ? {
+		id: vehicle.id, 
+		label: `${vehicle.brand} ${vehicle.model} - ${vehicle.license_plate}`
+	}:{});
+
+	const vehicleOptions = useMemo(() => [...[vehicleToOption(user.Vehicle)], ...vehicles.map(vehicleToOption)], [vehicles, user]);
+	const selectedVehicle = useMemo(() => vehicleToOption(user.Vehicle), [user]);
+
 	return (
 		<div>
 			<LogoutButton />
@@ -21,7 +36,8 @@ export default function DriverSection() {
 
 				<Autocomplete
 					disablePortal
-					options={[]}
+					options={vehicleOptions}
+					value={selectedVehicle}
 					sx={{ width: '359px' }}
 					renderInput={params => (
 						<TextField
